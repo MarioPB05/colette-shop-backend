9d87710f6ec32a7433b232628f64c62513af7c50
DavidClase <dzamoramartinez@safareyes.es>
2025-01-28 20:43:40 +0100

    ✨ Create brawler queries

--- src/Repository/BrawlerRepository.php
+++ src/Repository/BrawlerRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Brawler;
+use App\Entity\UserBrawler;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,35 @@ class BrawlerRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Brawler::class);
+    }
+
+    /**
+     * It counts the number of people who have the favorite brawler
+     * @return Brawler[]
+     */
+    public function findCountPersonByFavouriteBrawler(int $brawlerId): int
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = 'SELECT COUNT(u.user_id) FROM user_favorite_brawlers u WHERE u.brawler_id = :brawlerId';
+
+        $result = $conn->executeQuery($sql, ['brawlerId' => $brawlerId]);
+
+        return $result->fetchOne();
+    }
+
+    /**
+     * It counts the number of brawlers that a person has
+     * @return UserBrawler[]
+     */
+    public function findCountBrawlersByPerson(int $brawlerId): int
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = 'SELECT COUNT(u.user_id) FROM user_brawler u WHERE u.brawler_id = :brawlerId';
+
+        $result = $conn->executeQuery($sql, ['brawlerId' => $brawlerId]);
+
+        return $result->fetchOne();
     }
 }
