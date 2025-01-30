8eea6d62755278e47945bc5469820259f3cb8cb5
DavidClase <dzamoramartinez@safareyes.es>
2025-01-30 19:49:04 +0100

    ♻️ COmplete de DTO

--- src/DTO/user/TableUserResponse.php
+++ src/DTO/user/TableUserResponse.php
@@ -9,13 +9,15 @@ class TableUserResponse
     public string $username;
     public string $email;
     public int $gems;
+    public bool $enabled;
 
-    public function __construct(int $id, string $name, string $username, string $email, int $gems)
+    public function __construct(int $id, string $name, string $username, string $email, int $gems, bool $enabled)
     {
         $this->id = $id;
         $this->name = $name;
         $this->username = $username;
         $this->email = $email;
         $this->gems = $gems;
+        $this->enabled = $enabled;
     }
 }
