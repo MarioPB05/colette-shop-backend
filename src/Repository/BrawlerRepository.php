6a92e5a688294a2b7541697813c569cf2125e4b3
DavidClase <dzamoramartinez@safareyes.es>
2025-01-30 19:28:29 +0100

    🔧 Change name query

--- src/Repository/BrawlerRepository.php
+++ src/Repository/BrawlerRepository.php
@@ -20,17 +20,17 @@ class BrawlerRepository extends ServiceEntityRepository
     public function getAllBrawlers(): array
     {
         $conn = $this->getEntityManager()->getConnection();
-        $sql = 'SELECT b.id, b.name, b.pin_image, r.name AS rarity_name,
+        $sql = 'SELECT b.id, b.name, b.pin_image as pin_image, r.name AS rarity,
                     (
                         SELECT COUNT(u.user_id)
                         FROM user_brawler u
                         WHERE u.brawler_id = b.id
-                    ) AS count_brawlers,
+                    ) AS num_people,
                     (
                         SELECT COUNT(u.user_id)
                         FROM user_favorite_brawlers u
                         WHERE u.brawler_id = b.id
-                    ) AS count_person_favourite_brawler
+                    ) AS num_favourite
                 FROM brawler AS b
                 JOIN rarity AS r ON b.rarity_id = r.id';
 
