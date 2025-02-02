f6724b9e6cbc39359893e45bfc247b745b9fd794
Mario <mperdiguerobarrera@safareyes.es>
2025-02-02 14:01:57 +0100

    ♻️ Refactor getAllBoxes controller

--- src/Controller/BoxController.php
+++ src/Controller/BoxController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\DTO\box\TableBoxResponse;
 use App\Repository\BoxRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
-#[Route('/api/box')]
+#[Route('/api/boxes')]
 final class BoxController extends AbstractController{
 
     #[Route('/', name: 'box_get_all', methods: ['GET'])]
-    public function getAll(BoxRepository $boxRepository): JsonResponse
+    public function getAll(BoxRepository $boxRepository, TranslatorInterface $translator): JsonResponse
     {
-        return $this->json($boxRepository->getAllBoxes());
+        return $this->json(array_map(fn($result) => new TableBoxResponse(
+            $result['id'],
+            $result['name'],
+            $result['price'],
+            $result['quantity'],
+            $result['type'],
+            $result['pinned'],
+            $translator
+        ), $boxRepository->getAllBoxes()));
     }
 
 }
