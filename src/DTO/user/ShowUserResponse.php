a6cd86d3d0a127fd4cc32f7e728a488a44aa2e55
david <ticotico6365@gmail.com>
2025-02-04 17:37:52 +0100

    ♻️ add new variable

--- src/DTO/user/ShowUserResponse.php
+++ src/DTO/user/ShowUserResponse.php
@@ -7,18 +7,23 @@ class ShowUserResponse
     public int $id;
     public string $name;
     public string $surname;
+    public string $brawlTag;
     public string $username;
     public string $email;
     public int $gems;
     public bool $enabled;
+    public string $avatar;
 
-    public function __construct(int $id, string $name, string $username, string $email, int $gems, bool $enabled)
+    public function __construct(int $id, string $name, string $surname, string $brawlTag, string $username, string $email, int $gems, bool $enabled, string $avatar)
     {
         $this->id = $id;
         $this->name = $name;
+        $this->surname = $surname;
+        $this->brawlTag = $brawlTag;
         $this->username = $username;
         $this->email = $email;
         $this->gems = $gems;
         $this->enabled = $enabled;
+        $this->avatar = $avatar;
     }
 }
