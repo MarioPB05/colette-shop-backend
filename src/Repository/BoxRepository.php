d741a7deaa65217fd36f59a9b105bc35ec36f65e
DavidClase <dperezfernandez@safareyes.es>
2025-02-04 21:00:52 +0100

    ✨ Create repository for getting all shop boxes

--- src/Repository/BoxRepository.php
+++ src/Repository/BoxRepository.php
@@ -2,8 +2,11 @@
 
 namespace App\Repository;
 
+use App\DTO\box\BoxShopResponse;
 use App\Entity\Box;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,11 +19,25 @@ class BoxRepository extends ServiceEntityRepository
         parent::__construct($registry, Box::class);
     }
 
-    public function getAllBoxesShop()
+    /**
+     * It returns all the boxes that are going to be shown in the shop
+     *
+     * @param User $user
+     * @return array<BoxShopResponse>
+     * @throws Exception
+     */
+    public function getAllBoxesShop(User $user): array
     {
         $conn = $this->getEntityManager()->getConnection();
-        $sql = 'SELECT b.id, b.name, b.price, b.type FROM box b';
-        $result = $conn->executeQuery($sql);
+        $sql = 'SELECT b.id, b.name, b.price, b.type, b.quantity as boxesLeft, count(ufb.brawler_id) as favoriteBrawlersInBox, b.pinned, (COUNT(i.id) > 100) AS popular
+                FROM box b
+                LEFT JOIN box_brawler bb on b.id = bb.box_id
+                LEFT JOIN user_favorite_brawlers ufb on bb.brawler_id = ufb.brawler_id and ufb.user_id = :userId
+                LEFT JOIN inventory i on b.id = i.box_id
+                WHERE b.deleted = FALSE
+                GROUP BY b.id';
+
+        $result = $conn->executeQuery($sql, ['userId' => $user->getId()]);
         return $result->fetchAllAssociative();
     }
 
