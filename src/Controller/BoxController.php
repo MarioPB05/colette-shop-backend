20683ba3afb608075f95778c1bf4cb467c1a46a2
Mario <mperdiguerobarrera@safareyes.es>
2025-02-07 15:11:09 +0100

    🐛 Fix route bug

--- src/Controller/BoxController.php
+++ src/Controller/BoxController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-#[Route('/api/box')]
+#[Route('/api/boxes')]
 final class BoxController extends AbstractController {
 
     #[Route('/', name: 'box_get_all', methods: ['GET'])]
