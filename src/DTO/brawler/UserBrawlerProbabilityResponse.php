3a381395a98f726e10ce126ca9d8f70d049b5770
DavidClase <dperezfernandez@safareyes.es>
2025-02-24 21:26:29 +0100

    ♻️ Change name of user brawler probability field

--- src/DTO/brawler/UserBrawlerProbabilityResponse.php
+++ src/DTO/brawler/UserBrawlerProbabilityResponse.php
@@ -9,17 +9,17 @@ class UserBrawlerProbabilityResponse
     public string $image;
     public string $model_image;
     public int $probability;
-    public int $quantity;
+    public int $user_quantity;
     public int $rarity_id;
 
-    public function __construct(int $id, string $name, string $image, string $model_image, int $probability, int $quantity, int $rarity_id)
+    public function __construct(int $id, string $name, string $image, string $model_image, int $probability, int $user_quantity, int $rarity_id)
     {
         $this->id = $id;
         $this->name = $name;
         $this->image = $image;
         $this->model_image = $model_image;
         $this->probability = $probability;
-        $this->quantity = $quantity;
+        $this->user_quantity = $user_quantity;
         $this->rarity_id = $rarity_id;
     }
 }
