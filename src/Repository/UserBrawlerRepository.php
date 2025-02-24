cac62a4679a577924316f4554b3897beadad759b
david <ticotico6365@gmail.com>
2025-02-24 14:59:53 +0100

    ✨ Create new query

--- src/Repository/UserBrawlerRepository.php
+++ src/Repository/UserBrawlerRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repository;
 
+use App\Entity\Brawler;
+use App\Entity\User;
 use App\Entity\UserBrawler;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,5 +16,27 @@ class UserBrawlerRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, UserBrawler::class);
+    }
+
+    /**
+     * Returns the brawlers that the user has.
+     * @param User $user
+     * @return Brawler[]
+     */
+    public function getBrawlers(User $user): Brawler
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+            select ub.brawler_id, b.name, b.image, b.model_image
+                from user_brawler ub
+                left join public.brawler b on b.id = ub.brawler_id
+                where ub.user_id = :userId;";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute(['userId' => $user->getId()]);
+
+        return $stmt->fetchAllAssociative();
     }
 }
