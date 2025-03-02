86b13df395c8c590728caf2d5b7edef75ef03a89
MarioPB05 <101870138+MarioPB05@users.noreply.github.com>
2025-03-02 13:13:43 +0100

    ♻️ Change response status

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -134,7 +134,7 @@ final class UserController extends AbstractController{
         $user = $this->getUser();
 
         if (!$user) {
-            return new JsonResponse(['message' => 'User not found'], Response::HTTP_OK);
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_UNAUTHORIZED);
         }
 
         $brawlers = $userBrawlerRepository->getBrawlers($user);
