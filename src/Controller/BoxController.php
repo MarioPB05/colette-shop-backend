bbfdfb32f21438ebed7825b8ec26a8df54a9e487
Mario <mperdiguerobarrera@safareyes.es>
2025-03-02 22:55:00 +0100

    ✨ Create method to covert frontend cart to details cart

--- src/Controller/BoxController.php
+++ src/Controller/BoxController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\DTO\box\BoxCartRequest;
+use App\DTO\box\BoxCartResponse;
 use App\DTO\box\BoxDetailResponse;
 use App\DTO\box\BoxShopResponse;
 use App\DTO\box\DailyBoxShopResponse;
@@ -13,6 +15,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -68,6 +71,52 @@ final class BoxController extends AbstractController
             $result['pinned'],
             $translator
         ), $boxRepository->getAllBoxes()));
+    }
+
+    #[Route('/cart', name: 'box_get_from_cart', methods: ['POST'])]
+    public function getBoxesFromCart(#[MapRequestPayload] BoxCartRequest $request, BoxRepository $boxRepository, TranslatorInterface $translator): JsonResponse
+    {
+        // Flat all box id in array
+        $boxIds = array_map(fn($item) => $item->boxId, $request->items);
+
+        // Get all boxes from database
+        $boxes = $boxRepository->getAllBoxDetails($boxIds);
+
+        /** @var BoxCartResponse[] $boxes */
+        $response = [];
+
+        foreach ($boxes as $box) {
+            $item = array_values(array_filter($request->items, fn($item) => $item->boxId === $box['id']))[0];
+
+            // Check if it has enough quantity in database
+            if ($box['boxes_left'] != -1 && $box['boxes_left'] < $item->quantity) {
+                // Set quantity to maximum available, or if not available, remove from cart
+                $item->quantity = $box['boxes_left'];
+                if ($item->quantity === 0) {
+                    continue;
+                }
+            }
+
+            // Check if box is deleted
+            if ($box['deleted']) {
+                continue;
+            }
+
+            $response[] = new BoxCartResponse(
+                $box['id'],
+                $box['name'],
+                $box['type'],
+                $box['price'],
+                $item->quantity,
+                $box['price'] * $item->quantity,
+                $box['boxes_left'],
+                $box['is_daily'],
+                $box['claimed'],
+                $translator
+            );
+        }
+
+        return $this->json($response);
     }
 
     #[Route('/{id}', name: 'box_remove', methods: ['DELETE'])]
