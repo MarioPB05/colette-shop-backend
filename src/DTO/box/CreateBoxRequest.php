ee342e0e951b6eb98f30ac1147822f4feefbbb6f
david <dperezfernandez@safareyes.es>
2025-03-04 12:59:47 +0100

    ✨ Create 'create box' dto

---
+++ src/DTO/box/CreateBoxRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DTO\box;
+
+use App\Enum\BoxType;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class CreateBoxRequest
+{
+    public string $name;
+    public int $price;
+    public int $type;
+    public int $quantity;
+    public int $brawler_quantity;
+    public array $brawlers_in_box;
+
+    public function __construct(string $name, int $price, int $type, int $quantity, int $brawler_quantity, array $brawlers_in_box)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->type = $type + 1;
+        $this->quantity = $quantity;
+        $this->brawler_quantity = $brawler_quantity;
+        $this->brawlers_in_box = $brawlers_in_box;
+    }
+}
