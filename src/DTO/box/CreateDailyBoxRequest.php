4c8972d287eaa5cc5b526880f477d476bd2d9875
david <dperezfernandez@safareyes.es>
2025-03-04 12:59:54 +0100

    ✨ Create 'create daily box' dto

---
+++ src/DTO/box/CreateDailyBoxRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTO\box;
+
+class CreateDailyBoxRequest
+{
+    public string $name;
+    public int $type;
+    public int $repeat_every_hours;
+    public int $brawler_quantity;
+    public array $brawlers_in_box;
+
+    public function __construct(string $name, int $type, int $repeat_every_hours, int $brawler_quantity, array $brawlers_in_box)
+    {
+        $this->name = $name;
+        $this->type = $type + 1;
+        $this->repeat_every_hours = $repeat_every_hours;
+        $this->brawler_quantity = $brawler_quantity;
+        $this->brawlers_in_box = $brawlers_in_box;
+    }
+}
